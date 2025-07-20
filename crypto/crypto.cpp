4665748220e114037dd1d042e60ff10bf8bf9952
ASSh Dev <175926883+assh-dev@users.noreply.github.com>
2025-07-20 11:42:59 +0530

    Remove environment variable thingy

--- crypto/crypto.cpp
+++ crypto/crypto.cpp
@@ -20,19 +20,30 @@ void fetch_value_of_generator(mp_int& generator){
 
 void fetch_value_of_big_prime_number(mp_int& big_prime_number) {
     /*
-     * Initializes a big prime number for mod operator later in public key function
-     * This big prime is taken from rfc 7919 and is stored in the environment variables
+     * Initializes a big prime number (RFC 7919 group 4096-bit) for use in modular operations
+     * Source: https://datatracker.ietf.org/doc/html/rfc7919#appendix-A.1
      */
-    const char* p_env = "p_variable";
-    const char* p_value = getenv(p_env);
+    const char* rfc7919_prime_hex = 
+        "FFFFFFFFFFFFFFFFADF85458A2BB4A9AAFDC5620273D3CF1"
+        "D8B9C583CE2D3695A9E13641146433FBCC939DCE249B3EF9"
+        "7D2FE363630C75D8F681B202AEC4617AD3DF1ED5D5FD6561"
+        "2433F51F5F066ED0856365553DED1AF3B557135E7F57C935"
+        "984F0C70E0E68B77E2A689DAF3EFE8721DF158A136ADE735"
+        "30ACCA4F483A797ABC0AB182B324FB61D108A94BB2C8E3FB"
+        "B96ADAB760D7F4681D4F42A3DE394DF4AE56EDE76372BB19"
+        "0B07A7C8EE0A6D709E02FCE1CDF7E2ECC03404CD28342F61"
+        "9172FE9CE98583FF8E4F1232EEF28183C3FE3B1B4C6FAD73"
+        "3BB5FCBC2EC22005C58EF1837D1683B2C6F34A26C1B2EFFA"
+        "886B423861285C97FFFFFFFFFFFFFFFF";
 
-    if(mp_init(&big_prime_number) != MP_OKAY){
+    if (mp_init(&big_prime_number) != MP_OKAY) {
         printf("Error initializing Big Prime\n");
+        exit(1);
     }
 
-    mp_err reading_prime_from_env = mp_read_radix(&big_prime_number, p_value, 16);
-    if(reading_prime_from_env != MP_OKAY){
-        printf("Error reading Big Prime: %s\n", mp_error_to_string(reading_prime_from_env));
+    mp_err err = mp_read_radix(&big_prime_number, rfc7919_prime_hex, 16);
+    if (err != MP_OKAY) {
+        printf("Error reading Big Prime: %s\n", mp_error_to_string(err));
         exit(1);
     }
 }
